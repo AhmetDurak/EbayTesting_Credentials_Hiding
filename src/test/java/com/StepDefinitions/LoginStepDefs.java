2a1f77f5293125aab8d0f5e4f6edce859ff07dc7
Adrk51 <durakahmet049@gmail.com>
2022-07-05 18:21:33 +0200

    changes

--- src/test/java/com/StepDefinitions/LoginStepDefs.java
+++ src/test/java/com/StepDefinitions/LoginStepDefs.java
@@ -1,6 +1,7 @@
 package com.StepDefinitions;
 
 import com.Pages.LoginPage;
+import com.Utilities.BrowserUtilities;
 import io.cucumber.java.en.Given;
 
 public class LoginStepDefs {
@@ -8,7 +9,9 @@ public class LoginStepDefs {
 
     @Given("User should log in valid credentials")
     public void user_should_log_in_valid_credentials() {
+        BrowserUtilities.waitFor(3);
         ebay = new LoginPage();
+        BrowserUtilities.waitFor(3);
         ebay.consent.click();
         ebay.login.click();
 
