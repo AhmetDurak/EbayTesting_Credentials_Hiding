e726e72fbe221eb44ae179baa7624b47310b0b0b
Ahmet Durak <98670034+AhmetDurak@users.noreply.github.com>
2022-07-06 16:22:22 +0200

    Delete script.groovy

--- script.groovy
+++
@@ -1,8 +0,0 @@
-def addCredentials(){
-    withCredentials([usernamePassword(credentialsId: 'JenkinsCredentialGlobal', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-    echo "username: ${USER}"
-    echo "password: ${PASS}"
-  }
-}
-
-return this
