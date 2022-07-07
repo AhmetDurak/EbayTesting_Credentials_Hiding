1d855f625456535d42cdd05083a3afe4e525c28b
Adrk51 <durakahmet049@gmail.com>
2022-07-07 21:25:50 +0200

    only show Cred
    Signed-off-by: Adrk51 <durakahmet049@gmail.com>

--- src/test/java/com/Pages/PageBase.java
+++ src/test/java/com/Pages/PageBase.java
@@ -26,8 +26,8 @@ public class PageBase {
      * This method will return a valid credential as a tester
      */
     public void credentials(){
-        String username = EncryptionUtils.decrypt("pxWP45eecjK6gSys6j5BKFP8EUDYnp/QNdVu7x7tVVc=", "ahmet");
-        String password = EncryptionUtils.decrypt("YmxjvVNiGgF9hePnl/hbJw==", "ahmet");
+        String username = EncryptionUtils.decrypt("pxWP45eecjK6gSys6j5BKFP8EUDYnp/QNdVu7x7tVVc=", "selman");
+        String password = EncryptionUtils.decrypt("YmxjvVNiGgF9hePnl/hbJw==", "selman");
         this.username.sendKeys(username);
         this.password.sendKeys(password + Keys.ENTER);
 
