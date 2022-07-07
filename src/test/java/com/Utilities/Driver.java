36da4b54f9be251344bf3e4ba5045d5a655a3e43
Adrk51 <durakahmet049@gmail.com>
2022-07-07 15:52:11 +0200

    remote-chrome is added to Driver for Selenium-Grid

--- src/test/java/com/Utilities/Driver.java
+++ src/test/java/com/Utilities/Driver.java
@@ -9,14 +9,18 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 
 public class Driver {
 
-    private Driver() {}
+    private Driver() {
+    }
 
     //private static WebDriver driver;
 
@@ -33,6 +37,18 @@ public class Driver {
                     driverPool.set(new ChromeDriver(options));
                     //driver.manage().window().maximize();
                     driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "remote-chrome":
+                    try {
+                        // assign your grid server address
+                        String gridAddress = "23.22.37.164";
+                        URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
+                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                        desiredCapabilities.setBrowserName("chrome");
+                        driverPool.set(new RemoteWebDriver(url, desiredCapabilities));
+                        driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    } catch (Exception ignored) {
+                    }
                     break;
                 case "chrome-headless":
                     WebDriverManager.chromedriver().setup();
@@ -84,25 +100,26 @@ public class Driver {
     }
 }
 
-class myDriver{
+class myDriver {
     /*
-    * Creating a private constructor, so we are closing
-    * access to the object of this class from outside the class
-    */
-    private myDriver(){}
+     * Creating a private constructor, so we are closing
+     * access to the object of this class from outside the class
+     */
+    private myDriver() {
+    }
 
     /*
-    * We make Webdriver private, because we want to close access from outside the class.
-    * We make it static because we use it in a static method*/
+     * We make Webdriver private, because we want to close access from outside the class.
+     * We make it static because we use it in a static method*/
     private static WebDriver driver;
 
     // Create a re-usable utility method which will return same driver instance when we call it
-    public static WebDriver get(){
-        if (driver == null){
+    public static WebDriver get() {
+        if (driver == null) {
             String browser = ConfigurationReader.get("url");
             // Depending on the browserType that will be return from configuration.properties file
             // switch statement will determine the case, and open the matching browser
-            switch (browser){
+            switch (browser) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
